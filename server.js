de3aa0ecf7e5ec33b81e967582460ce14881a453
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-02-23 16:29:54 -0500

    homepage view added

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// handlebars needed for set up
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+// handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 // turn on routes
 app.use(routes);
+
 
 // turn on connection to db and server
 // By forcing the sync method to true, the tables re-create if there are any association changes.
