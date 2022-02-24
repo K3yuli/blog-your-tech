58ed238f2759a8567310596ee023f816d3aa5089
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-02-24 13:14:55 -0500

    single post hamdlebars

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -6,16 +6,20 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    // check the session
+    if(req.session){
     Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
+        user_id: req.body.post_id,
+        // use id from session
+        post_id: req.session.user_id
     })
     .then(dbCommentData => res.json(dbCommentData))
     .cath(err => {
         console.log(err);
         res.status(400).json(err);
     });
+    }
 });
 
 router.delete('/:id', (req, res) => {
