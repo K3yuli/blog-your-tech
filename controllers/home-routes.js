ae6b51ae7d5ed8b6ab79cabe180a84f4541371cd
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-04-06 12:26:17 -0400

    home-routes updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,4 @@
+const req = require('express/lib/request');
 const res = require('express/lib/response');
 const sequelize = require('../config/connection');
 const {Post, User, Comment } = require('../models');
@@ -28,6 +29,7 @@ router.get('/', (req, res) => {
     })
     .then(dbPostData => {
         // pass single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('homepage', { 
             posts,
             loggedIn: req.session.loggedIn
@@ -48,6 +50,10 @@ router.get('/login', (req, res) => {
     
     res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+})
 
 router.get('/post/:id', (req, res) => {
 Post.findOne({
@@ -83,7 +89,7 @@ Post.findOne({
 
     // serialize the data
     const post = dbPostData.get({ plain: true });
-
+    console.log(post);
     // pass data to template
     res.render('single-post', { 
     post,
@@ -97,5 +103,43 @@ Post.findOne({
     });
 
 });
+
+router.get('/posts-comments', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+    {
+        model: User,
+        attributes: ['username']
+    }]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id.'});
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+        res.render('posts_comments', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 module.exports = router;
