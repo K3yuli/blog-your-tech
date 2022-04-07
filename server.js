78b6fa62c0b6965abeaf5cb96a6ae132d2786bec
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-04-07 17:58:23 -0400

    server update

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ 
+  helpers: {
+    format_date: date => {
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  }
+}
+});
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -28,7 +34,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
@@ -37,5 +43,5 @@ app.use(require('./controllers/'));
 // turn on connection to db and server
 // By forcing the sync method to true, the tables re-create if there are any association changes.
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
